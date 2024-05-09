43e5ed52458c1f0ee5513e95f6e20c52809e807d
Tony Shen <fengzhizi715@126.com>
2024-05-09 16:02:27 +0800

    add OilPaintFilter and EmbossFilter

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/imageprocess/filter/EmbossFilter.kt
@@ -0,0 +1,64 @@
+package cn.netdiscovery.monica.imageprocess.filter
+
+import cn.netdiscovery.monica.imageprocess.filter.base.ColorProcessorFilter
+import cn.netdiscovery.monica.utils.clamp
+import java.awt.image.BufferedImage
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.imageprocess.filter.EmbossFilter
+ * @author: Tony Shen
+ * @date: 2024/5/9 11:01
+ * @version: V1.0 <描述当前版本功能>
+ */
+class EmbossFilter(private val COLORCONSTANTS:Int = 100, private val out:Boolean = false): ColorProcessorFilter() {
+
+    override fun doColorProcessor(dstImage: BufferedImage): BufferedImage {
+        val outPixels = IntArray(width * height)
+
+        var offset = 0
+        var r1 = 0
+        var g1 = 0
+        var b1 = 0
+        var r2 = 0
+        var g2 = 0
+        var b2 = 0
+        var r = 0
+        var g = 0
+        var b = 0
+
+        for (y in 1 until height - 1) {
+            offset = y * width
+            var ta = 0
+            for (x in 1 until width - 1) {
+                r1 = R[offset].toInt() and 0xff
+                g1 = G[offset].toInt() and 0xff
+                b1 = B[offset].toInt() and 0xff
+                r2 = R[offset + width].toInt() and 0xff
+                g2 = G[offset + width].toInt() and 0xff
+                b2 = B[offset + width].toInt() and 0xff
+                if (out) {
+                    r = r1 - r2
+                    g = g1 - g2
+                    b = b1 - b2
+                } else {
+                    r = r2 - r1
+                    g = g2 - g1
+                    b = b2 - b1
+                }
+                r = clamp(r + COLORCONSTANTS)
+                g = clamp(g + COLORCONSTANTS)
+                b = clamp(b + COLORCONSTANTS)
+
+                ta = inPixels[offset] shr 24 and 0xff
+                outPixels[offset] = ta shl 24 or (r shl 16) or (g shl 8) or b
+
+                offset++
+            }
+        }
+
+        setRGB(dstImage, 0, 0, width, height, outPixels)
+        return dstImage
+    }
+}
---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/imageprocess/filter/OilPaintFilter.kt
@@ -0,0 +1,91 @@
+package cn.netdiscovery.monica.imageprocess.filter
+
+import cn.netdiscovery.monica.imageprocess.filter.base.BaseFilter
+import java.awt.image.BufferedImage
+
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.imageprocess.filter.OilPaintFilter
+ * @author: Tony Shen
+ * @date: 2024/5/8 20:38
+ * @version: V1.0 <描述当前版本功能>
+ */
+class OilPaintFilter(private val ksize:Int = 10,private val intensity:Int = 40): BaseFilter() {
+
+    override fun doFilter(srcImage: BufferedImage, dstImage: BufferedImage): BufferedImage {
+
+        val inPixels = IntArray(width * height)
+        val outPixels = IntArray(width * height)
+        getRGB(srcImage, 0, 0, width, height, inPixels)
+        var index = 0
+        val subradius: Int = this.ksize / 2
+        val intensityCount = IntArray((intensity + 1).toInt())
+        val ravg = IntArray((intensity + 1).toInt())
+        val gavg = IntArray((intensity + 1).toInt())
+        val bavg = IntArray((intensity + 1).toInt())
+        for (i in 0..intensity.toInt()) {
+            intensityCount[i] = 0
+            ravg[i] = 0
+            gavg[i] = 0
+            bavg[i] = 0
+        }
+        for (row in 0 until height) {
+            val ta = 0
+            var tr = 0
+            var tg = 0
+            var tb = 0
+            for (col in 0 until width) {
+                for (subRow in -subradius..subradius) {
+                    for (subCol in -subradius..subradius) {
+                        var nrow = row + subRow
+                        var ncol = col + subCol
+                        if (nrow >= height || nrow < 0) {
+                            nrow = 0
+                        }
+                        if (ncol >= width || ncol < 0) {
+                            ncol = 0
+                        }
+                        index = nrow * width + ncol
+                        tr = inPixels[index] shr 16 and 0xff
+                        tg = inPixels[index] shr 8 and 0xff
+                        tb = inPixels[index] and 0xff
+                        val curIntensity = (((tr + tg + tb) / 3).toDouble() * intensity / 255.0f).toInt()
+                        intensityCount[curIntensity]++
+                        ravg[curIntensity] += tr
+                        gavg[curIntensity] += tg
+                        bavg[curIntensity] += tb
+                    }
+                }
+
+                // find the max number of same gray level pixel
+                var maxCount = 0
+                var maxIndex = 0
+                for (m in intensityCount.indices) {
+                    if (intensityCount[m] > maxCount) {
+                        maxCount = intensityCount[m]
+                        maxIndex = m
+                    }
+                }
+
+                // get average value of the pixel
+                val nr = ravg[maxIndex] / maxCount
+                val ng = gavg[maxIndex] / maxCount
+                val nb = bavg[maxIndex] / maxCount
+                index = row * width + col
+                outPixels[index] = ta shl 24 or (nr shl 16) or (ng shl 8) or nb
+
+                // post clear values for next pixel
+                for (i in 0..intensity.toInt()) {
+                    intensityCount[i] = 0
+                    ravg[i] = 0
+                    gavg[i] = 0
+                    bavg[i] = 0
+                }
+            }
+        }
+        setRGB(dstImage, 0, 0, width, height, outPixels)
+        return dstImage
+    }
+}
