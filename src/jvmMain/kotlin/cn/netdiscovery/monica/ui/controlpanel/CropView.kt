c99a0b41ef27a434202081264ea67e58ddcdf691
Tony Shen <fengzhizi715@126.com>
2024-05-10 19:42:14 +0800

    Update CropView.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/CropView.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/CropView.kt
@@ -1,20 +1,23 @@
 package cn.netdiscovery.monica.ui.controlpanel
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Checkbox
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cn.netdiscovery.monica.state.ApplicationState
+import cn.netdiscovery.monica.utils.click
 import org.koin.compose.koinInject
 
 /**
@@ -25,6 +28,8 @@ import org.koin.compose.koinInject
  * @date: 2024/5/7 13:56
  * @version: V1.0 <描述当前版本功能>
  */
+var clickStatus = mutableStateOf(0)
+
 @Composable
 fun cropView(state: ApplicationState) {
     val viewModel:CropViewModel = koinInject()
@@ -32,6 +37,10 @@ fun cropView(state: ApplicationState) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Checkbox(state.isCrop, onCheckedChange = {
             state.isCrop = it
+
+            if (!state.isCrop) {
+                clearClickStatus()
+            }
         })
         Text("裁剪", color = Color.Black, fontSize = 20.sp)
     }
@@ -42,6 +51,7 @@ fun cropView(state: ApplicationState) {
         IconButton(
             modifier = Modifier.padding(5.dp),
             onClick = {
+                clearClickStatus()
                 viewModel.flip(state)
             },
             enabled = state.isCrop
@@ -56,6 +66,7 @@ fun cropView(state: ApplicationState) {
         IconButton(
             modifier = Modifier.padding(5.dp),
             onClick = {
+                clearClickStatus()
                 viewModel.rotate(state)
             },
             enabled = state.isCrop
@@ -70,7 +81,7 @@ fun cropView(state: ApplicationState) {
         IconButton(
             modifier = Modifier.padding(5.dp),
             onClick = {
-
+                clickStatus.value = 1
             },
             enabled = state.isCrop
         ) {
@@ -79,6 +90,82 @@ fun cropView(state: ApplicationState) {
                 contentDescription = "缩放",
                 modifier = Modifier.size(36.dp)
             )
+        }
+
+    }
+
+    Column {
+        if (clickStatus.value == 1 && state.currentImage!=null) {
+            generateResizeParams(state)
+        }
+    }
+
+}
+
+private fun clearClickStatus() {
+    clickStatus.value = 0
+}
+
+@Composable
+fun generateResizeParams(state: ApplicationState) {
+
+    var widthText by remember {
+        mutableStateOf("${state.currentImage?.width?:400}")
+    }
+
+    var heightText by remember {
+        mutableStateOf("${state.currentImage?.height?:400}")
+    }
+
+    Row(
+        modifier = Modifier.padding(top = 10.dp)
+    ) {
+        Text(text = "width")
+
+        BasicTextField(
+            value = widthText,
+            onValueChange = { str ->
+                widthText = str
+            },
+            keyboardOptions = KeyboardOptions.Default,
+            keyboardActions = KeyboardActions.Default,
+            cursorBrush = SolidColor(Color.Gray),
+            singleLine = true,
+            modifier = Modifier.padding(start = 10.dp).width(120.dp).background(Color.LightGray.copy(alpha = 0.5f), shape = RoundedCornerShape(3.dp)).height(20.dp),
+            textStyle = TextStyle(Color.Black, fontSize = 12.sp)
+        )
+    }
+
+    Row(
+        modifier = Modifier.padding(top = 10.dp)
+    ) {
+        Text(text = "height")
+
+        BasicTextField(
+            value = heightText,
+            onValueChange = { str ->
+                heightText = str
+            },
+            keyboardOptions = KeyboardOptions.Default,
+            keyboardActions = KeyboardActions.Default,
+            cursorBrush = SolidColor(Color.Gray),
+            singleLine = true,
+            modifier = Modifier.padding(start = 10.dp).width(120.dp).background(Color.LightGray.copy(alpha = 0.5f), shape = RoundedCornerShape(3.dp)).height(20.dp),
+            textStyle = TextStyle(Color.Black, fontSize = 12.sp)
+        )
+
+        Row {
+            Button(
+                modifier = Modifier.offset(x = 140.dp,y = 0.dp),
+                onClick = {
+                    click {
+                    }
+                },
+                enabled = state.isCrop
+            ) {
+                Text(text = "确定",
+                color = if (state.isCrop) Color.Unspecified else Color.LightGray)
+            }
         }
 
     }
