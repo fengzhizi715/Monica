c36c26dde1a1f2ad9be82c2d9c397c8346d9100b
Tony Shen <fengzhizi715@126.com>
2024-05-14 21:30:14 +0800

    update

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/image/gesture/MotionEvent.kt
@@ -0,0 +1,13 @@
+package cn.netdiscovery.monica.ui.image.gesture
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.ui.image.gesture.MotionEvent
+ * @author: Tony Shen
+ * @date: 2024/5/14 16:00
+ * @version: V1.0 <描述当前版本功能>
+ */
+enum class MotionEvent {
+    Idle, Down, Move, Up
+}
