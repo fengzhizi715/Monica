b1325a48e2b9f23ff668103c880bd9957a69c104
Tony Shen <fengzhizi715@126.com>
2024-05-30 11:02:11 +0800

    Update DoodleViewModel.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/doodle/DoodleViewModel.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/doodle/DoodleViewModel.kt
@@ -1,12 +1,9 @@
 package cn.netdiscovery.monica.ui.controlpanel.doodle
 
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.toAwtImage
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import cn.netdiscovery.monica.state.ApplicationState
@@ -44,15 +41,28 @@ class DoodleViewModel {
                 val path = it.first
                 val property = it.second
 
-                drawPath(
-                    color = property.color,
-                    path = path,
-                    style = Stroke(
-                        width = property.strokeWidth,
-                        cap = property.strokeCap,
-                        join = property.strokeJoin
+                if (!property.eraseMode) {
+                    drawPath(
+                        color = property.color,
+                        path = path,
+                        style = Stroke(
+                            width = property.strokeWidth,
+                            cap = property.strokeCap,
+                            join = property.strokeJoin
+                        )
                     )
-                )
+                } else {
+                    drawPath(
+                        color = Color.Transparent,
+                        path = path,
+                        style = Stroke(
+                            width = property.strokeWidth,
+                            cap = property.strokeCap,
+                            join = property.strokeJoin
+                        ),
+                        blendMode = BlendMode.Clear
+                    )
+                }
             }
         }
 
