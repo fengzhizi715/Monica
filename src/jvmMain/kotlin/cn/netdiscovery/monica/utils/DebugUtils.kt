bbed77d5793b1b02bd9914cb4b8dfc6340e64b02
Tony Shen <fengzhizi715@126.com>
2024-06-07 18:11:39 +0800

    Update DebugUtils.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/utils/DebugUtils.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/utils/DebugUtils.kt
@@ -4,6 +4,8 @@ import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
 import javax.imageio.ImageIO
+import kotlin.coroutines.RestrictsSuspension
+import kotlin.system.measureTimeMillis
 
 /**
  *
@@ -13,7 +15,35 @@ import javax.imageio.ImageIO
  * @date: 2024/4/30 12:43
  * @version: V1.0 <描述当前版本功能>
  */
+
+/**
+ * 把 BufferedImage 转换成 文件，便于调试时使用
+ */
 @Throws(IOException::class)
 fun writeImageFile(bi: BufferedImage, fileName:String) {
     ImageIO.write(bi, "png", File(fileName))
+}
+
+/**
+ * 统计耗时任务的时间
+ */
+fun measure(block: () -> Unit):Long {
+
+    val timeCost = measureTimeMillis {
+        block.invoke()
+    }
+
+    return timeCost
+}
+
+/**
+ * 统计耗时任务的时间
+ */
+suspend fun measureWithSuspend(block: suspend() -> Unit):Long {
+
+    val timeCost = measureTimeMillis {
+        block.invoke()
+    }
+
+    return timeCost
 }
