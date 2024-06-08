bb821ec063d284115218e7f88a1c64b07b6d60da
Tony Shen <fengzhizi715@126.com>
2024-06-08 22:11:02 +0800

    Update ConBriFilter.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/imageprocess/filter/ConBriFilter.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/imageprocess/filter/ConBriFilter.kt
@@ -1,6 +1,6 @@
 package cn.netdiscovery.monica.imageprocess.filter
 
-import cn.netdiscovery.monica.imageprocess.filter.base.BaseFilter
+import cn.netdiscovery.monica.imageprocess.filter.base.ColorProcessorFilter
 import cn.netdiscovery.monica.utils.clamp
 import java.awt.image.BufferedImage
 
@@ -12,12 +12,9 @@ import java.awt.image.BufferedImage
  * @date: 2024/4/27 13:57
  * @version: V1.0 <描述当前版本功能>
  */
-class ConBriFilter(private val contrast:Float = 1.5f,private val brightness:Float =1.0f): BaseFilter() {
+class ConBriFilter(private val contrast:Float = 1.5f,private val brightness:Float =1.0f): ColorProcessorFilter() {
 
-    override fun doFilter(srcImage: BufferedImage, dstImage: BufferedImage): BufferedImage {
-
-        val outPixels = IntArray(width * height)
-
+    override fun doColorProcessor(dstImage: BufferedImage): BufferedImage {
         // calculate RED, GREEN, BLUE means of pixel
         var index = 0
         val rgbmeans = IntArray(3)
@@ -26,16 +23,15 @@ class ConBriFilter(private val contrast:Float = 1.5f,private val brightness:Floa
         var blueSum = 0.0
         val total = (height * width).toDouble()
         for (row in 0 until height) {
-            var ta = 0
             var tr = 0
             var tg = 0
             var tb = 0
             for (col in 0 until width) {
                 index = row * width + col
-                ta = inPixels[index] shr 24 and 0xff
-                tr = inPixels[index] shr 16 and 0xff
-                tg = inPixels[index] shr 8 and 0xff
-                tb = inPixels[index] and 0xff
+
+                tr = R[index].toInt() and 0xff
+                tg = G[index].toInt() and 0xff
+                tb = B[index].toInt() and 0xff
                 redSum += tr.toDouble()
                 greenSum += tg.toDouble()
                 blueSum += tb.toDouble()
@@ -48,16 +44,15 @@ class ConBriFilter(private val contrast:Float = 1.5f,private val brightness:Floa
 
         // adjust contrast and brightness algorithm, here
         for (row in 0 until height) {
-            var ta = 0
             var tr = 0
             var tg = 0
             var tb = 0
             for (col in 0 until width) {
                 index = row * width + col
-                ta = inPixels[index] shr 24 and 0xff
-                tr = inPixels[index] shr 16 and 0xff
-                tg = inPixels[index] shr 8 and 0xff
-                tb = inPixels[index] and 0xff
+
+                tr = R[index].toInt() and 0xff
+                tg = G[index].toInt() and 0xff
+                tb = B[index].toInt() and 0xff
 
                 // remove means
                 tr -= rgbmeans[0]
@@ -71,11 +66,13 @@ class ConBriFilter(private val contrast:Float = 1.5f,private val brightness:Floa
                 tr += rgbmeans[0] * brightness.toInt()
                 tg += rgbmeans[1] * brightness.toInt()
                 tb += rgbmeans[2] * brightness.toInt()
-                outPixels[index] = ta shl 24 or (clamp(tr) shl 16) or (clamp(tg) shl 8) or clamp(tb)
+
+                R[index] = clamp(tr).toByte()
+                G[index] = clamp(tg).toByte()
+                B[index] = clamp(tb).toByte()
             }
         }
 
-        setRGB(dstImage, 0, 0, width, height, outPixels)
-        return dstImage
+        return toBufferedImage(dstImage)
     }
 }
