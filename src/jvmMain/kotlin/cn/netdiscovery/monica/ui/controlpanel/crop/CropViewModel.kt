1231e723e5c257683800baef3df115b5d8419b57
Tony Shen <fengzhizi715@126.com>
2024-06-12 17:57:32 +0800

    Update CropViewModel.kt
    删除冗余的代码

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/crop/CropViewModel.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/crop/CropViewModel.kt
@@ -1,16 +1,16 @@
 package cn.netdiscovery.monica.ui.controlpanel.crop
 
-import androidx.compose.ui.geometry.Offset
 import cn.netdiscovery.monica.config.KEY_CROP_FIRST
 import cn.netdiscovery.monica.config.KEY_CROP_SECOND
-import cn.netdiscovery.monica.imageprocess.*
+import cn.netdiscovery.monica.imageprocess.flipHorizontally
+import cn.netdiscovery.monica.imageprocess.resize
+import cn.netdiscovery.monica.imageprocess.rotate
 import cn.netdiscovery.monica.rxcache.rxCache
 import cn.netdiscovery.monica.state.ApplicationState
 import cn.netdiscovery.monica.ui.controlpanel.crop.cropimage.contentScalesIndex
 import cn.netdiscovery.monica.ui.controlpanel.crop.cropimage.cropFlag1
 import cn.netdiscovery.monica.ui.controlpanel.crop.cropimage.cropFlag2
 import cn.netdiscovery.monica.ui.controlpanel.crop.cropimage.cropTypesIndex
-import kotlin.math.abs
 
 /**
  *
@@ -21,8 +21,6 @@ import kotlin.math.abs
  * @version: V1.0 <描述当前版本功能>
  */
 class CropViewModel {
-
-    private var offset:Offset = Offset.Zero
 
     fun flip(state: ApplicationState) {
         if (state.currentImage!=null) {
@@ -48,35 +46,6 @@ class CropViewModel {
             val resizedImage = state.currentImage!!.resize(width, height)
             state.addQueue(state.currentImage!!)
             state.currentImage = resizedImage
-        }
-    }
-
-    fun crop(width:Int, height:Int, offset: Offset, state: ApplicationState) {
-
-        if (this.offset == Offset.Zero) {
-            this.offset = offset
-            return
-        }
-
-        if (this.offset != offset) {
-            val bufferedImage = state.currentImage!!
-            val srcWidth = bufferedImage.width
-            val srcHeight = bufferedImage.height
-
-            val xScale = (srcWidth.toFloat()/width)
-            val yScale = (srcHeight.toFloat()/height)
-
-            // 打码区域左上角x坐标
-            val x = (this.offset.x*xScale).toInt()
-            // 打码区域左上角y坐标
-            val y = (this.offset.y*yScale).toInt()
-
-            val w = abs(offset.x*xScale - x)
-            val h = abs(offset.y*xScale - y)
-
-            state.addQueue(state.currentImage!!)
-            state.currentImage = state.currentImage!!.subImage(x,y,w.toInt(),h.toInt())
-            this.offset = Offset.Zero
         }
     }
 
