704a5bc28c6ea1d32e95fc1875126745dd08b3ff
Tony Shen <fengzhizi715@126.com>
2024-06-16 19:04:13 +0800

    update

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/imageprocess/lut/LUT.kt
@@ -0,0 +1,41 @@
+package cn.netdiscovery.monica.imageprocess.lut
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.imageprocess.lut.LUT
+ * @author: Tony Shen
+ * @date: 2024/6/16 15:43
+ * @version: V1.0 <描述当前版本功能>
+ */
+const val AUTUMN_STYLE: Int = 0
+const val BONE_STYLE = 1
+const val COOL_STYLE = 2
+const val HOT_STYLE = 3
+const val HSV_STYLE = 4
+const val JET_STYLE = 5
+const val OCEAN_STYLE = 6
+const val PINK_STYLE = 7
+const val RAINBOW_STYLE = 8
+const val SPRING_STYLE = 9
+const val SUMMER_STYLE = 10
+const val WINTER_STYLE = 11
+
+fun getColorFilterLUT(style: Int): Array<IntArray> {
+
+    return when (style) {
+        AUTUMN_STYLE -> AUTUMN_LUT
+        BONE_STYLE -> BONE_LUT
+        COOL_STYLE -> COOL_LUT
+        HOT_STYLE -> HOT_LUT
+        HSV_STYLE -> HSV_LUT
+        JET_STYLE -> JET_LUT
+        OCEAN_STYLE -> OCEAN_LUT
+        PINK_STYLE -> PINK_LUT
+        RAINBOW_STYLE -> RAINBOW_LUT
+        SPRING_STYLE -> SPRING_LUT
+        SUMMER_STYLE -> SUMMER_LUT
+        WINTER_STYLE -> WINTER_LUT
+        else -> AUTUMN_LUT
+    }
+}
