83ab6c998df7e9e57a29c444c3125392003f9ddd
Tony Shen <fengzhizi715@126.com>
2024-06-16 12:44:47 +0800

    update

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/doodle/ColorSelectionDialog.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/doodle/ColorSelectionDialog.kt
@@ -1,6 +1,5 @@
 package cn.netdiscovery.monica.ui.controlpanel.doodle
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,16 +9,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.StrokeCap
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
-import cn.netdiscovery.monica.ui.controlpanel.doodle.model.PathProperties
 import kotlin.math.roundToInt
 
 /**
@@ -31,111 +25,6 @@ import kotlin.math.roundToInt
  * @version: V1.0 <描述当前版本功能>
  */
 val Blue400 = Color(0xff42A5F5)
-
-@Composable
-fun PropertiesMenuDialog(pathOption: PathProperties, onDismiss: () -> Unit) {
-
-    var strokeWidth by remember { mutableStateOf(pathOption.strokeWidth) }
-    var strokeCap by remember { mutableStateOf(pathOption.strokeCap) }
-    var strokeJoin by remember { mutableStateOf(pathOption.strokeJoin) }
-
-    Dialog(onDismissRequest = onDismiss) {
-
-        Card(
-            elevation = 2.dp,
-            shape = RoundedCornerShape(8.dp),
-            modifier = Modifier.padding(vertical = 8.dp)
-        ) {
-            Column(modifier = Modifier.padding(8.dp)) {
-
-                Text(
-                    text = "Properties",
-                    color = Blue400,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(start = 12.dp, top = 12.dp)
-                )
-
-                Canvas(
-                    modifier = Modifier
-                        .padding(horizontal = 24.dp, vertical = 20.dp)
-                        .height(40.dp)
-                        .fillMaxWidth()
-                ) {
-                    val path = Path()
-                    path.moveTo(0f, size.height / 2)
-                    path.lineTo(size.width, size.height / 2)
-
-                    drawPath(
-                        color = pathOption.color,
-                        path = path,
-                        style = Stroke(
-                            width = strokeWidth,
-                            cap = strokeCap,
-                            join = strokeJoin
-                        )
-                    )
-                }
-
-                Text(
-                    text = "Stroke Width ${strokeWidth.toInt()}",
-                    fontSize = 16.sp,
-                    modifier = Modifier.padding(horizontal = 12.dp)
-                )
-
-                Slider(
-                    value = strokeWidth,
-                    onValueChange = {
-                        strokeWidth = it
-                        pathOption.strokeWidth = strokeWidth
-                    },
-                    valueRange = 1f..100f,
-                    onValueChangeFinished = {}
-                )
-
-                ExposedSelectionMenu(title = "Stroke Cap",
-                    index = when (strokeCap) {
-                        StrokeCap.Butt -> 0
-                        StrokeCap.Round -> 1
-                        else -> 2
-                    },
-                    options = listOf("Butt", "Round", "Square"),
-                    onSelected = {
-                        println("STOKE CAP $it")
-                        strokeCap = when (it) {
-                            0 -> StrokeCap.Butt
-                            1 -> StrokeCap.Round
-                            else -> StrokeCap.Square
-                        }
-
-                        pathOption.strokeCap = strokeCap
-
-                    }
-                )
-
-                ExposedSelectionMenu(title = "Stroke Join",
-                    index = when (strokeJoin) {
-                        StrokeJoin.Miter -> 0
-                        StrokeJoin.Round -> 1
-                        else -> 2
-                    },
-                    options = listOf("Miter", "Round", "Bevel"),
-                    onSelected = {
-                        println("STOKE JOIN $it")
-
-                        strokeJoin = when (it) {
-                            0 -> StrokeJoin.Miter
-                            1 -> StrokeJoin.Round
-                            else -> StrokeJoin.Bevel
-                        }
-
-                        pathOption.strokeJoin = strokeJoin
-                    }
-                )
-            }
-        }
-    }
-}
 
 @Composable
 fun ColorSelectionDialog(
---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/doodle/PropertiesMenuDialog.kt
@@ -0,0 +1,136 @@
+package cn.netdiscovery.monica.ui.controlpanel.doodle
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import cn.netdiscovery.monica.ui.controlpanel.doodle.model.PathProperties
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.ui.controlpanel.doodle.PropertiesMenuDialog
+ * @author: Tony Shen
+ * @date: 2024/6/16 12:38
+ * @version: V1.0 <描述当前版本功能>
+ */
+
+@Composable
+fun PropertiesMenuDialog(pathOption: PathProperties, onDismiss: () -> Unit) {
+
+    var strokeWidth by remember { mutableStateOf(pathOption.strokeWidth) }
+    var strokeCap by remember { mutableStateOf(pathOption.strokeCap) }
+    var strokeJoin by remember { mutableStateOf(pathOption.strokeJoin) }
+
+    Dialog(onDismissRequest = onDismiss) {
+
+        Card(
+            elevation = 2.dp,
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier.padding(vertical = 8.dp)
+        ) {
+            Column(modifier = Modifier.padding(8.dp)) {
+
+                Text(
+                    text = "Properties",
+                    color = Blue400,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 12.dp, top = 12.dp)
+                )
+
+                Canvas(
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp, vertical = 20.dp)
+                        .height(40.dp)
+                        .fillMaxWidth()
+                ) {
+                    val path = Path()
+                    path.moveTo(0f, size.height / 2)
+                    path.lineTo(size.width, size.height / 2)
+
+                    drawPath(
+                        color = pathOption.color,
+                        path = path,
+                        style = Stroke(
+                            width = strokeWidth,
+                            cap = strokeCap,
+                            join = strokeJoin
+                        )
+                    )
+                }
+
+                Text(
+                    text = "Stroke Width ${strokeWidth.toInt()}",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+
+                Slider(
+                    value = strokeWidth,
+                    onValueChange = {
+                        strokeWidth = it
+                        pathOption.strokeWidth = strokeWidth
+                    },
+                    valueRange = 1f..100f,
+                    onValueChangeFinished = {}
+                )
+
+                ExposedSelectionMenu(title = "Stroke Cap",
+                    index = when (strokeCap) {
+                        StrokeCap.Butt -> 0
+                        StrokeCap.Round -> 1
+                        else -> 2
+                    },
+                    options = listOf("Butt", "Round", "Square"),
+                    onSelected = {
+                        println("STOKE CAP $it")
+                        strokeCap = when (it) {
+                            0 -> StrokeCap.Butt
+                            1 -> StrokeCap.Round
+                            else -> StrokeCap.Square
+                        }
+
+                        pathOption.strokeCap = strokeCap
+
+                    }
+                )
+
+                ExposedSelectionMenu(title = "Stroke Join",
+                    index = when (strokeJoin) {
+                        StrokeJoin.Miter -> 0
+                        StrokeJoin.Round -> 1
+                        else -> 2
+                    },
+                    options = listOf("Miter", "Round", "Bevel"),
+                    onSelected = {
+                        println("STOKE JOIN $it")
+
+                        strokeJoin = when (it) {
+                            0 -> StrokeJoin.Miter
+                            1 -> StrokeJoin.Round
+                            else -> StrokeJoin.Bevel
+                        }
+
+                        pathOption.strokeJoin = strokeJoin
+                    }
+                )
+            }
+        }
+    }
+}
