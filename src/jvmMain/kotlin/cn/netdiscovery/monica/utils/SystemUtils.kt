2be146cc87356318d63a943ee671bf3833de1fc2
Tony Shen <fengzhizi715@126.com>
2024-07-06 14:41:45 +0800

    Create SystemUtils.kt

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/utils/SystemUtils.kt
@@ -0,0 +1,16 @@
+package cn.netdiscovery.monica.utils
+
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.utils.SystemUtils
+ * @author: Tony Shen
+ * @date:  2024/7/6 14:36
+ * @version: V1.0 <描述当前版本功能>
+ */
+
+val os = System.getProperty("os.name")
+val arch = System.getProperty("os.arch")
+val osVersion = System.getProperty("os.version")
+val javaVersion = System.getProperty("java.version")
