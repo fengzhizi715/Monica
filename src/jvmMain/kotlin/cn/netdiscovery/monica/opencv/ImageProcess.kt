d8d366c3d73ce67a0f9df3e14584c066e4128963
Tony Shen <fengzhizi715@126.com>
2024-07-14 22:32:34 +0800

    Create ImageProcess.kt

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/opencv/ImageProcess.kt
@@ -0,0 +1,15 @@
+package cn.netdiscovery.monica.opencv
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.opencv.ImageProcess
+ * @author: Tony Shen
+ * @date:  2024/7/14 21:22
+ * @version: V1.0 <描述当前版本功能>
+ */
+object ImageProcess {
+
+
+    external fun getOpenCVVersion()
+}
