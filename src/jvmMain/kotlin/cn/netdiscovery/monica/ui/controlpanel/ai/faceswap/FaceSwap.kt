d2164d83cfec22deb8203b4f9a46c40ac4e9f0e2
Tony Shen <fengzhizi715@126.com>
2024-08-25 14:53:09 +0800

    Update FaceSwap.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/faceswap/FaceSwap.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/faceswap/FaceSwap.kt
@@ -3,13 +3,11 @@ package cn.netdiscovery.monica.ui.controlpanel.ai.faceswap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -17,6 +15,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cn.netdiscovery.monica.state.ApplicationState
+import cn.netdiscovery.monica.utils.composeClick
 
 /**
  *
@@ -30,59 +29,75 @@ import cn.netdiscovery.monica.state.ApplicationState
 @Composable
 fun faceSwap(state: ApplicationState) {
 
-    Row (
-        modifier = Modifier.fillMaxSize().padding(top = 50.dp, bottom = 80.dp, start = 20.dp, end = 20.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Card(
-            modifier = Modifier.padding(16.dp).weight(1.0f),
-            shape = RoundedCornerShape(8.dp),
-            elevation = 4.dp,
-            onClick = {
-//            previewViewModel.chooseImage(state)
-            },
-            enabled = state.rawImage == null
+    Column (modifier = Modifier.fillMaxSize()){
+        Row (
+            modifier = Modifier.fillMaxSize().weight(9.5f).padding(top= 20.dp, start = 20.dp, end = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-//            Text(
-//                modifier = Modifier.padding(vertical = 1.dp),
-//                text = "source",
-//                color = MaterialTheme.colors.primary,
-//                fontSize = 20.sp,
-//                fontWeight = FontWeight.Bold
-//            )
-
-            Column(
-                modifier = Modifier,
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Card(
+                modifier = Modifier.padding(16.dp).weight(1.0f),
+                shape = RoundedCornerShape(8.dp),
+                elevation = 4.dp,
+                onClick = {
+//            previewViewModel.chooseImage(state)
+                },
+                enabled = state.rawImage == null
             ) {
-                Image(
-                    painter = state.currentImage!!.toPainter(),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier)
+                Column(
+                    modifier = Modifier,
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        modifier = Modifier,
+                        text = "source",
+                        color = MaterialTheme.colors.primary,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Image(
+                        painter = state.currentImage!!.toPainter(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier)
+                }
+            }
+
+            Card(
+                modifier = Modifier.padding(16.dp).weight(1.0f),
+                shape = RoundedCornerShape(8.dp),
+                elevation = 4.dp,
+                onClick = {
+                },
+                enabled = state.rawImage == null
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "请点击选择图像或拖拽图像至此",
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
 
-        Card(
-            modifier = Modifier.padding(16.dp).weight(1.0f),
-            shape = RoundedCornerShape(8.dp),
-            elevation = 4.dp,
-            onClick = {
-//            previewViewModel.chooseImage(state)
-            },
-            enabled = state.rawImage == null
+        Row(modifier = Modifier.fillMaxSize().weight(0.5f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Button(
+                modifier = Modifier.width(100.dp),
+                onClick = composeClick {
+                },
+                enabled = true
             ) {
-                Text(
-                    text = "请点击选择图像或拖拽图像至此",
-                    textAlign = TextAlign.Center
-                )
+                Text(text = "确定",
+                    color = if (true) Color.Unspecified else Color.LightGray)
             }
         }
     }
