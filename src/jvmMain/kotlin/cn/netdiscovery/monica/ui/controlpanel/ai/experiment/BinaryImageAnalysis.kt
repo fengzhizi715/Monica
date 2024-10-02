bfafb0521bbaf4320355c862754d1dc0505a652d
Tony Shen <fengzhizi715@126.com>
2024-10-02 23:57:37 +0800

    Update BinaryImageAnalysis.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
@@ -1,15 +1,20 @@
 package cn.netdiscovery.monica.ui.controlpanel.ai.experiment
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cn.netdiscovery.monica.config.subTitleTextSize
+import cn.netdiscovery.monica.state.BlurStatus
 import cn.netdiscovery.monica.ui.widget.divider
 import cn.netdiscovery.monica.ui.widget.subTitle
+import cn.netdiscovery.monica.ui.widget.toolTipButton
+import cn.netdiscovery.monica.utils.composeClick
 
 /**
  *
@@ -23,12 +28,21 @@ import cn.netdiscovery.monica.ui.widget.subTitle
 fun binaryImageAnalysis() {
 
     Column (modifier = Modifier.fillMaxSize().padding(start = 20.dp, end =  20.dp)) {
-        Column(modifier = Modifier.padding(top = 20.dp).weight(0.2f)) {
+        Column(modifier = Modifier.padding(top = 20.dp).weight(0.15f)) {
             subTitle(text = "灰度图像", color = Color.Black)
             divider()
+
+            Button(
+                modifier = Modifier,
+                onClick = composeClick {
+
+                }
+            ) {
+                Text(text = "图像灰度化", color = Color.Unspecified)
+            }
         }
 
-        Column(modifier = Modifier.weight(0.3f)) {
+        Column(modifier = Modifier.weight(0.35f)) {
             subTitle(text = "阈值分割", color = Color.Black)
             divider()
         }
