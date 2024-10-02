10e862d0f6f7e2d790a0fe75066fc45b771da69f
Tony Shen <fengzhizi715@126.com>
2024-10-03 00:34:00 +0800

    Update BinaryImageAnalysis.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
@@ -2,6 +2,7 @@ package cn.netdiscovery.monica.ui.controlpanel.ai.experiment
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,14 +11,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import cn.netdiscovery.monica.config.subTitleTextSize
-import cn.netdiscovery.monica.state.BlurStatus
 import cn.netdiscovery.monica.ui.widget.divider
 import cn.netdiscovery.monica.ui.widget.subTitle
-import cn.netdiscovery.monica.ui.widget.toolTipButton
 import cn.netdiscovery.monica.utils.composeClick
 
 /**
@@ -52,9 +48,13 @@ fun binaryImageAnalysis() {
             val typeSelectedOption = remember { mutableStateOf(false) }
             val thresholdSelectedOption = remember { mutableStateOf(false) }
 
-            Row {
-                Text("阈值化类型: ", modifier = Modifier.align(Alignment.CenterVertically))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(true, onCheckedChange = {
+                })
+                Text("阈值化类型", modifier = Modifier.align(Alignment.CenterVertically))
+            }
 
+            Row {
                 RadioButton(
                     selected = !typeSelectedOption.value,
                     onClick = { typeSelectedOption.value = false }
@@ -69,9 +69,13 @@ fun binaryImageAnalysis() {
                 Text("THRESH_BINARY_INV", modifier = Modifier.align(Alignment.CenterVertically))
             }
 
-            Row {
-                Text("全局阈值分割: ", modifier = Modifier.align(Alignment.CenterVertically))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(true, onCheckedChange = {
+                })
+                Text("全局阈值分割", modifier = Modifier.align(Alignment.CenterVertically))
+            }
 
+            Row {
                 RadioButton(
                     selected = !thresholdSelectedOption.value,
                     onClick = { thresholdSelectedOption.value = false }
@@ -83,6 +87,12 @@ fun binaryImageAnalysis() {
                     onClick = { thresholdSelectedOption.value = true }
                 )
                 Text("THRESH_TRIANGLE", modifier = Modifier.align(Alignment.CenterVertically))
+            }
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Checkbox(true, onCheckedChange = {
+                })
+                Text("自适应阈值分割", modifier = Modifier.align(Alignment.CenterVertically))
             }
         }
 
