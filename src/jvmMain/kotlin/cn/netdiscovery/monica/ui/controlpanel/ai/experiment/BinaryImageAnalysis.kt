df63fc8938f44ac8b5e7de16b6aa1ad3562d975b
Tony Shen <fengzhizi715@126.com>
2024-10-04 14:04:57 +0800

    Update BinaryImageAnalysis.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/BinaryImageAnalysis.kt
@@ -1,17 +1,23 @@
 package cn.netdiscovery.monica.ui.controlpanel.ai.experiment
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Checkbox
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cn.netdiscovery.monica.state.ApplicationState
 import cn.netdiscovery.monica.ui.widget.divider
 import cn.netdiscovery.monica.ui.widget.subTitle
@@ -134,6 +140,58 @@ fun binaryImageAnalysis(state: ApplicationState) {
 
                     Text(text = it, modifier = Modifier.align(Alignment.CenterVertically))
                 }
+            }
+
+
+            var blockSizeText by remember {
+                mutableStateOf("")
+            }
+
+            var cText by remember {
+                mutableStateOf("")
+            }
+
+            Row {
+                Text(text = "blockSize")
+
+                BasicTextField(
+                    value = blockSizeText,
+                    onValueChange = { str ->
+                        blockSizeText = str
+                    },
+                    keyboardOptions = KeyboardOptions.Default,
+                    keyboardActions = KeyboardActions.Default,
+                    cursorBrush = SolidColor(Color.Gray),
+                    singleLine = true,
+                    modifier = Modifier.padding(start = 10.dp).width(120.dp).background(Color.LightGray.copy(alpha = 0.5f), shape = RoundedCornerShape(3.dp)).height(20.dp),
+                    textStyle = TextStyle(Color.Black, fontSize = 12.sp)
+                )
+            }
+
+            Row(modifier = Modifier.padding(top = 10.dp)) {
+                Text(text = "c")
+
+                BasicTextField(
+                    value = cText,
+                    onValueChange = { str ->
+                        cText = str
+                    },
+                    keyboardOptions = KeyboardOptions.Default,
+                    keyboardActions = KeyboardActions.Default,
+                    cursorBrush = SolidColor(Color.Gray),
+                    singleLine = true,
+                    modifier = Modifier.padding(start = 10.dp).width(120.dp).background(Color.LightGray.copy(alpha = 0.5f), shape = RoundedCornerShape(3.dp)).height(20.dp),
+                    textStyle = TextStyle(Color.Black, fontSize = 12.sp)
+                )
+            }
+
+            Button(
+                modifier = Modifier,
+                onClick = composeClick {
+
+                }
+            ) {
+                Text(text = "阈值分割", color = Color.Unspecified)
             }
         }
 
