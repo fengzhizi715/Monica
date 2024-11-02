3aa5c8bda7a630365ac0b1ef6f87d1e873539554
Tony Shen <fengzhizi715@126.com>
2024-11-02 18:06:45 +0800

    Update ExperimentHome.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/ExperimentHome.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/ExperimentHome.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import cn.netdiscovery.monica.ui.widget.subTitle
-import cn.netdiscovery.monica.ui.widget.title
 
 /**
  *
@@ -27,22 +24,15 @@ import cn.netdiscovery.monica.ui.widget.title
 @Composable
 fun experimentHome() {
 
-    Column (modifier = Modifier.fillMaxSize().padding(start = 20.dp, end =  20.dp, top = 10.dp)) {
-        title(modifier = Modifier.align(Alignment.CenterHorizontally) , text = "首页", color = Color.Black)
-
-        Column (
-            modifier = Modifier.fillMaxSize(),
+    Column (modifier = Modifier.fillMaxSize().padding(start = 20.dp, end =  20.dp, top = 10.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            Image(painter = painterResource("images/ai/OpenCV_Logo.png"),
-                contentDescription = null,
-                modifier = Modifier)
+    ) {
+        Image(painter = painterResource("images/ai/OpenCV_Logo.png"),
+            contentDescription = null,
+            modifier = Modifier)
 
-            subTitle(modifier = Modifier.padding(top = 20.dp),
-                    text = "本模块的算法使用 OpenCV C++ 实现，目前只适用于一些简单 CV 算法的快速验证")
-
-        }
-
+        subTitle(modifier = Modifier.padding(top = 20.dp),
+            text = "本模块的算法使用 OpenCV C++ 实现，目前只适用于一些简单 CV 算法的快速验证")
     }
 }
