d2177151e3e792dcc662002b02c6d72fc8eff47b
Tony Shen <fengzhizi715@126.com>
2024-12-21 20:17:02 +0800

    update

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/ExperimentView.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/ExperimentView.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import cn.netdiscovery.monica.imageprocess.BufferedImages
 import cn.netdiscovery.monica.state.ApplicationState
+import cn.netdiscovery.monica.ui.controlpanel.ai.experiment.model.morphologicalOperations
 import cn.netdiscovery.monica.ui.widget.*
 import cn.netdiscovery.monica.utils.Action
 import cn.netdiscovery.monica.utils.composeClick
@@ -104,6 +105,7 @@ fun customNavigationHost(
         }
 
         composable(Screen.MorphologicalOperations.name) {
+            morphologicalOperations(state, Screen.MorphologicalOperations.label)
         }
 
         composable(Screen.TemplateMatching.name) {
---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/model/MorphologicalOperationsView.kt
@@ -0,0 +1,22 @@
+package cn.netdiscovery.monica.ui.controlpanel.ai.experiment.model
+
+import androidx.compose.runtime.Composable
+import cn.netdiscovery.monica.state.ApplicationState
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.ui.controlpanel.ai.experiment.model.MorphologicalOperationsView
+ * @author: Tony Shen
+ * @date: 2024/12/21 20:16
+ * @version: V1.0 <描述当前版本功能>
+ */
+private val logger: Logger = LoggerFactory.getLogger(object : Any() {}.javaClass.enclosingClass)
+
+
+@Composable
+fun morphologicalOperations(state: ApplicationState, title: String) {
+
+}
