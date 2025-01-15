94b476333006d9ed20d24caf6ccccb6b4b710983
Tony Shen <fengzhizi715@126.com>
2025-01-15 19:15:49 +0800

    Update MatchTemplateSettings.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/model/MatchTemplateSettings.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/ui/controlpanel/ai/experiment/model/MatchTemplateSettings.kt
@@ -8,5 +8,9 @@ package cn.netdiscovery.monica.ui.controlpanel.ai.experiment.model
  * @date: 2025/1/4 20:29
  * @version: V1.0 <描述当前版本功能>
  */
-class MatchTemplateSettings {
-}
+data class MatchTemplateSettings (
+    var matchType:Int = 0, // 0 表示原图匹配，1 表示灰度匹配 2 表示边缘匹配
+    var angleStart:Int,
+    var angleEnd:Int,
+    var angleStep:Int
+)
