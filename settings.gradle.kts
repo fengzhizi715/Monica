e035007667d49f39b4a4f95f2d2d3e6a7c537cb5
Tony Shen <fengzhizi715@126.com>
2025-02-11 11:32:13 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven( "https://jitpack.io" )
     }
 
     plugins {
@@ -13,4 +14,3 @@ pluginManagement {
 }
 
 rootProject.name = "Monica"
-
