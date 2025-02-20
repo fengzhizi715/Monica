f4e6643b7b2ade793bfd0d963102d91a0041624f
Tony Shen <fengzhizi715@126.com>
2025-02-20 17:55:36 +0800

    Update build.gradle.kts

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -1,9 +1,25 @@
 plugins {
     kotlin("jvm")
+    id("com.github.gmazzo.buildconfig") version "5.4.0"
 }
 
 repositories {
     mavenCentral()
+}
+
+val appVersion = "1.0.5"
+val isProVersion = true
+
+buildConfig {
+    useKotlinOutput { topLevelConstants = true }
+    useKotlinOutput { internalVisibility = false }   // adds `internal` modifier to all declarations
+
+    buildConfigField("APP_NAME", project.name)
+    buildConfigField("APP_VERSION", appVersion)
+    buildConfigField("KOTLIN_VERSION", "${rootProject.extra["kotlin.version"]}")
+    buildConfigField("COMPOSE_VERSION", "${rootProject.extra["compose.version"]}")
+    buildConfigField("IS_PRO_VERSION", isProVersion)
+    buildConfigField("BUILD_TIME", System.currentTimeMillis())
 }
 
 dependencies {
