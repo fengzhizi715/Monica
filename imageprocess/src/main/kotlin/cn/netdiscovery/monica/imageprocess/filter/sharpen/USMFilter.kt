e915214586e40031f97f2d2ce107e5d0cee9c101
Tony Shen <fengzhizi715@126.com>
2025-03-24 14:06:06 +0800

    Update USMFilter.kt

--- imageprocess/src/main/kotlin/cn/netdiscovery/monica/imageprocess/filter/sharpen/USMFilter.kt
+++ imageprocess/src/main/kotlin/cn/netdiscovery/monica/imageprocess/filter/sharpen/USMFilter.kt
@@ -34,14 +34,20 @@ class USMFilter(override val radius: Float =2f, private val amount: Float = 0.5f
                 var r1 = rgb1 shr 16 and 0xff
                 var g1 = rgb1 shr 8 and 0xff
                 var b1 = rgb1 and 0xff
+
                 val rgb2 = inPixels[index]
                 val r2 = rgb2 shr 16 and 0xff
                 val g2 = rgb2 shr 8 and 0xff
                 val b2 = rgb2 and 0xff
-                if (Math.abs(r1 - r2) >= threshold) r1 = clamp(((a + 1) * (r1 - r2) + r2).toInt())
-                if (Math.abs(g1 - g2) >= threshold) g1 = clamp(((a + 1) * (g1 - g2) + g2).toInt())
-                if (Math.abs(b1 - b2) >= threshold) b1 = clamp(((a + 1) * (b1 - b2) + b2).toInt())
-                inPixels[index] = rgb1 and -0x1000000 or (r1 shl 16) or (g1 shl 8) or b1
+
+                if (Math.abs(r1 - r2) >= threshold)
+                    r1 = clamp(((a + 1) * (r1 - r2) + r2).toInt())
+                if (Math.abs(g1 - g2) >= threshold)
+                    g1 = clamp(((a + 1) * (g1 - g2) + g2).toInt())
+                if (Math.abs(b1 - b2) >= threshold)
+                    b1 = clamp(((a + 1) * (b1 - b2) + b2).toInt())
+
+                inPixels[index] = rgb1 and 0xff000000.toInt() or (r1 shl 16) or (g1 shl 8) or b1
                 index++
             }
         }
