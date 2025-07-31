1ed688bf1c6ddacf204045fe18c559821618b42c
Tony Shen <fengzhizi715@126.com>
2025-07-31 17:49:07 +0800

    Update ColorCorrectionSettings.kt

--- domain/src/main/kotlin/cn/netdiscovery/monica/domain/ColorCorrectionSettings.kt
+++ domain/src/main/kotlin/cn/netdiscovery/monica/domain/ColorCorrectionSettings.kt
@@ -9,15 +9,15 @@ package cn.netdiscovery.monica.domain
  * @version: V1.0 <描述当前版本功能>
  */
 data class ColorCorrectionSettings(
-    val contrast:Int = 255,
-    val hue:Int = 180,
-    val saturation:Int = 255,
-    val lightness:Int = 255,
-    val temperature:Int = 255,
-    val highlight:Int = 255,
-    val shadow:Int = 255,
-    val sharpen:Int = 0,
-    val corner:Int = 0,
+    val contrast:Int = 255,     // 对比度，范围 0-510
+    val hue:Int = 180,          // 色调，范围 0-360
+    val saturation:Int = 255,   // 饱和度，范围 0-510
+    val lightness:Int = 255,    // 亮度，范围 0-510
+    val temperature:Int = 255,  // 色温，范围 0-510
+    val highlight:Int = 255,    // 高光，范围 0-510
+    val shadow:Int = 255,       // 阴影，范围 0-510
+    val sharpen:Int = 0,        // 锐化，范围 0-255
+    val corner:Int = 0,         // 暗角，范围 0-255
 
     val status:Int = 0 // 1 contrast, 2 hue, 3 saturation, 4 lightness, 5 temperature, 6 highlight, 7 shadow, 8 sharpen, 9 corner
 )
