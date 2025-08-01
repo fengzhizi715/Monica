16dcde2d2799396b8bab4414bc737be67d8e8636
Tony Shen <fengzhizi715@126.com>
2025-08-01 17:28:23 +0800

    Create DialogSession.kt

---
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/llm/DialogSession.kt
@@ -0,0 +1,17 @@
+package cn.netdiscovery.monica.llm
+
+import cn.netdiscovery.monica.domain.ColorCorrectionSettings
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.monica.llm.DialogSession
+ * @author: Tony Shen
+ * @date: 2025/8/1 17:27
+ * @version: V1.0 封装一个会话上下文类（保留系统提示 + 当前参数）
+ */
+data class DialogSession(
+    val systemPrompt: String,
+    var currentSettings: ColorCorrectionSettings,
+    val history: MutableList<Map<String, String>> = mutableListOf()
+)
