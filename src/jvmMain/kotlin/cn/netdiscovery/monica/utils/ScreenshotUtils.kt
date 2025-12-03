bd61753f216a44df40546099d69da0b3ab4fa214
fengzhizi715 <fengzhizi715@126.com>
2025-12-03 18:01:37 +0800

    Update ScreenshotUtils.kt

--- src/jvmMain/kotlin/cn/netdiscovery/monica/utils/ScreenshotUtils.kt
+++ src/jvmMain/kotlin/cn/netdiscovery/monica/utils/ScreenshotUtils.kt
@@ -1,7 +1,5 @@
 package cn.netdiscovery.monica.utils
 
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
 import cn.netdiscovery.monica.state.ApplicationState
 import cn.netdiscovery.monica.utils.extensions.launchWithLoading
 import org.slf4j.Logger
@@ -10,13 +8,12 @@ import java.awt.Rectangle
 import java.awt.Robot
 import java.awt.Toolkit
 import java.awt.image.BufferedImage
-import javax.imageio.ImageIO
 
 /**
  * 截图工具类
  * 
  * @author: Tony Shen
- * @date: 2025/11/09
+ * @date: 2025/12/03
  * @version: V1.0
  */
 private val logger: Logger = LoggerFactory.getLogger(object : Any() {}.javaClass.enclosingClass)
